73a5b1390449e2a565409f3a1d5032774442b679
malvarezg1 <60206865+malvarezg1@users.noreply.github.com>
2020-03-27 18:31:00 -0500

    Add files via upload

---
+++ tallerTypeScript/dataCourses.ts
@@ -0,0 +1,10 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Ingeniería de Sw", "El César", 3),
+  new Course("Finanzas", "Finanzas", 3),
+  new Course("Lenguajes y Maquinas", "La Taka Taka", 3),
+  new Course("Control de. Prod", "Alex", 3),
+  new Course("Hist. del Traje", "Juana y Monina", 2)
+]
